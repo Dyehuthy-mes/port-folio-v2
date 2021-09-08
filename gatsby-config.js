f8290e42ed3c76b4ec059e3933a78e29b2f46fab
Marcos Campot <campotelmarcos@gmail.com>
2021-09-08 19:30:21 -0300

    keep on goin¿

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,31 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sass",
-    "gatsby-plugin-react-helmet"
+    "gatsby-plugin-react-helmet",
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+        ignore: [`**/\.*`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: `png`, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
+    },
   ],
 };
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,43 +1,38 @@
 import React from 'react'
 import Header from './Header'
-import Marcos from '../images/sadas.png'
 import Img from 'gatsby-image'
 import {useStaticQuery, graphql} from 'gatsby'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
+export default Page
 
 function Page({children}) {
 
-/*     const data = useStaticQuery(graphql `
-        query{
-          placeholderImage: file(relativePath: { eq: "sadas.png"}) {
-            childImageSharp {
-              fluid(max-width:300) {
-                ...GatsbyImageSharpFluid
-              }
+    const data = useStaticQuery(graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "sadas.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 400, maxHeight: 400) {
+              ...GatsbyImageSharpFluid
             }
           }
         }
-        `)
-     */
-
+      }
+    `)
 
     return (
         <div className='wrapper'>
             <Header/>
-                <div className='imageController'>
-                    {/* <Img fluid={data.placeholderImage.childImageSharp.fluid}/> */}
-                    <img src= {Marcos} alt='sadasdas' width="200" height="300" />
-                </div>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-                in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, 
-                sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                
+              <div className="imgController">
+                <Img fluid={data.placeholderImage.childImageSharp.fluid}
+                  className="imgStyle"
+                  alt="marcoscampot"
+                />
+              </div>
+            <p> Hi there !! I'm a frontend developer from Buenos Aires. I am Currently research JS technologies</p> 
+            <a href= "https://www.linkedin.com/in/marcos-campot-687152216/" target="blank" > <FontAwesomeIcon icon={faLinkedin} /> </a>
+            <a href= "https://github.com/Dyehuthy-mes" target="blank" > <FontAwesomeIcon icon={faGithub} /> </a>
             {children}
         </div>
     )
-}
-
-export default Page
+  }
