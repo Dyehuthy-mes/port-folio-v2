c7c026103e18f06e062e768bf4142b5c0f5e4a28
Marcos Campot <campotelmarcos@gmail.com>
2021-12-28 18:01:58 -0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,10 @@
 import React from 'react'
 import '../styles/Header.css'
-import { useEffect, useState } from 'react';
 
 function Header() {
 
-  /*   const [checked, setOnclick] = useState(localStorage.getItem("theme") === "dark" ? true : false);
-
-    useEffect(() => {
-      document
-      .getElementsByTagName("HTML")[0]
-      .setAttribute("data-theme", localStorage.getItem("theme"));
-  }, [checked]);
-  
-  const toggleThemeChange = () => {
-    if (checked === false) {
-      localStorage.setItem("theme", "dark");
-      setOnclick(true);
-    } else {
-      localStorage.setItem("theme", "light");
-      setOnclick(false);
-    }
-  }; */
-
     return (
         <div className="header__title">
-{/*              <button
-               className='changeTheme'           
-               defaultChecked={checked}
-               onClick={() => toggleThemeChange()}
-             /> */}
             <h1>ברוך הבא</h1> 
         </div>
     )
